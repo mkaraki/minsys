48325e15ec339a6e9bb57786d3c7b3093219ac90
mkaraki <masakazu.araki@mkarakiapps.com>
2021-11-15 00:48:36 +0900

    Move ReadLine

--- UefiMain.c
+++ UefiMain.c
@@ -4,7 +4,38 @@
 // To call gBS (Global Boot Services)
 #include <Library/UefiBootServicesTableLib.h>
 
-void ReadLine(IN EFI_SYSTEM_TABLE *SystemTable, OUT CHAR16 *ReturnArray, int len);
+void ReadLine(IN EFI_SYSTEM_TABLE *SystemTable, OUT CHAR16 *ReturnArray, int len)
+{
+    SystemTable->ConOut->EnableCursor(SystemTable->ConOut, 1);
+
+    EFI_INPUT_KEY key;
+    for (int i = 0; i < len; i++)
+    {
+        UINTN KeyEvent;
+
+        SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &KeyEvent);
+        SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
+        SystemTable->ConIn->Reset(SystemTable->ConIn, 0);
+        Print(L"%c", key.UnicodeChar);
+
+        if (key.UnicodeChar == L'\r')
+        {
+            *(ReturnArray + i) = 0;
+            break;
+        }
+
+        if (key.UnicodeChar == 8 && i > 0)
+        {
+            *(ReturnArray + i - 1) = 0x00;
+            i -= 2;
+            continue;
+        }
+
+        *(ReturnArray + i) = key.UnicodeChar;
+    }
+    SystemTable->ConOut->EnableCursor(SystemTable->ConOut, 0);
+    Print(L"\n");
+}
 
 EFI_STATUS
 EFIAPI
@@ -38,37 +69,4 @@ UefiMain(
     }
 
     return EFI_SUCCESS;
-}
-
-void ReadLine(IN EFI_SYSTEM_TABLE *SystemTable, OUT CHAR16 *ReturnArray, int len)
-{
-    SystemTable->ConOut->EnableCursor(SystemTable->ConOut, 1);
-
-    EFI_INPUT_KEY key;
-    for (int i = 0; i < len; i++)
-    {
-        UINTN KeyEvent;
-
-        SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &KeyEvent);
-        SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
-        SystemTable->ConIn->Reset(SystemTable->ConIn, 0);
-        Print(L"%c", key.UnicodeChar);
-
-        if (key.UnicodeChar == L'\r')
-        {
-            *(ReturnArray + i) = 0;
-            break;
-        }
-
-        if (key.UnicodeChar == 8 && i > 0)
-        {
-            *(ReturnArray + i - 1) = 0x00;
-            i -= 2;
-            continue;
-        }
-
-        *(ReturnArray + i) = key.UnicodeChar;
-    }
-    SystemTable->ConOut->EnableCursor(SystemTable->ConOut, 0);
-    Print(L"\n");
 }
